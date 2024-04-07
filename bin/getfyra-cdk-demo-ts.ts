967341125cd5976218c81d44a507e054bcee6230
Malik Dixon <mdixon47@users.noreply.github.com>
2024-04-07 16:47:55 -0400

    first commit

--- bin/getfyra-cdk-demo-ts.ts
+++ bin/getfyra-cdk-demo-ts.ts
@@ -1,21 +1,13 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { GetfyraCdkDemoTsStack } from '../lib/getfyra-cdk-demo-ts-stack';
+import * as cdk from '@aws-cdk/core';
+import { S3BucketStack } from '../lib/getfyra-cdk-demo-ts-stack'; // Import the missing module
 
 const app = new cdk.App();
-new GetfyraCdkDemoTsStack(app, 'GetfyraCdkDemoTsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+// new GetfyraCdkDemoTsStack(app, 'GetfyraCdkDemoTsStack');
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// creating an s3 bucket stack
+const s3_bucket_stack = new S3BucketStack(app, 'getfryaS3Stack');
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// Reusing assess
+const bucket = s3_bucket_stack.bucket;
--- lib/getfyra-cdk-demo-ts-stack.ts
+++ lib/getfyra-cdk-demo-ts-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class GetfyraCdkDemoTsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class S3BucketStack extends cdk.Stack {
+  public readonly bucket: s3.Bucket;
 
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+      
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'GetfyraCdkDemoTsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+      this.bucket = new s3.Bucket(this, "getfrya-data-collection-dev", {
+          versioned: false,
+          bucketName: "getfrya-data-collection-dev",
+          publicReadAccess: false,
+          blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+          removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+    }
 }
